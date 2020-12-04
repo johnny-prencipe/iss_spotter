6ba39be3607d2268be4ec41ee24707340c611e05
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-04 02:38:43 +0000

    added LHLs tester code to my index file

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
+
+
+//might as well try with the example tester code too
+//it works
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
 // const myLocation = { latitude: '49.1044302', longitude: '-122.801094' };
 // fetchISSFlyOverTimes(myLocation, (error, data) => {
 //   if (error) {
